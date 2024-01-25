514d652415fa2ee6cabd7b58805208f8a8f6abc2
Olha <o.solovei.ua@gmail.com>
2024-01-25 10:57:27 +0200

    added skills section and styles

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,39 +1,65 @@
+import "../styles/skills.css"
+import { useInView } from "react-intersection-observer"
+import { useEffect, useState } from "react"
+
 function Skills() {
+  const [active, setActive] = useState(false)
+  const {ref, inView} = useInView({
+    threshold: 0.1
+  })
+    useEffect(() => {
+      if (inView) {
+        setActive(true)
+      }
+  }, [inView])
+
   return (
-    <div id='skills'>
+    <div id='skills'  ref={ref} className={active ? 'visible' : ''}>
       <h2>Skills</h2>
       <div className='skills'>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/js-img.jpg' alt='' />
+          <span>Javascript</span>
         </div>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/react-img.jpg' alt='' />
+          <span>React</span>
         </div>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/html-img.jpg' alt='' />
+          <span>HTML</span>
         </div>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/css-img.jpg' alt='' />
+          <span>CSS</span>
         </div>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/svelte-img.jpg' alt='' />
+          <span>Svelte</span>
         </div>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/tailwind-img.jpg' alt='' />
+          <span>CSS frameworks</span>
         </div>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/git-img.jpg' alt='' />
+          <span>Version Control/Git</span>
         </div>
-        <div className='skill'>
-          <img src='' alt='' />
-          <span></span>
+        <div className='skillbox'>
+          <img src='../skills/dom-img.jpg' alt='' />
+          <span>DOM Manipulation</span>
+        </div>
+        <div className='skillbox'>
+          <img src='../skills/figma-img.jpg' alt='' />
+          <span>Figma</span>
+        </div>
+        <div className='skillbox'>
+          <img src='../skills/responsive-img.jpg' alt='' />
+          <span>Responsive Design</span>
+        </div>
+        <div className='skillbox'>
+          <img src='../skills/inter-img.jpg' alt='' />
+          <span>Interpersonal Skills</span>
         </div>
       </div>
     </div>
