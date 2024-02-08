76cf70df2ab656c678542b71c28aaf979dfde948
Olha <o.solovei.ua@gmail.com>
2024-02-08 13:21:05 +0200

    added text about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,22 @@
-import styles from '../styles/About.module.css'
+import styles from "../styles/About.module.css"
 
 function About() {
   return (
     <div id={styles.about}>
-      <h2>About</h2>
+      <h2 id='nav-about'>About</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit, sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua. Ut enim ad minim veniam, quis
-        nostrud exercitation ullamco laboris nisi ut aliquip
-        ex ea commodo consequat. <span>Duis aute irure dolor in
-        reprehenderit in voluptate.</span> Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia
-        deserunt mollit anim id est laborum.
+        Proficient in adeptly resolving technical challenges
+        through hands-on experience in web development and
+        design. Possess a{" "}
+        <span>
+          Bachelor's degree in Computer Engineering
+        </span>
+        , complemented by internship participation in a
+        substantial commercial project within a
+        collaborative team environment. Demonstrates the
+        ability to promptly retrieve information to address
+        challenges and exhibits strong communication skills,
+        particularly when engaging with clients.
       </p>
     </div>
   )
