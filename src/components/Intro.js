b2ad319f889b9da792ba1b9a66ea8616f5e67fd1
Olha <o.solovei.ua@gmail.com>
2025-03-02 14:30:48 +0100

    styled intro text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,16 +9,15 @@ function Intro() {
 
   return (
     <div id='intro' className={extraClasses}>
-      <div>
+      <div className="intro-text">
         <p>
           I'm <span>Olha Solovei</span>,
         </p>
         <p>Frontend developer </p>
-        <p>(React.js) </p>
         <a href='#nav-contact'>Contact me</a>
       </div>
       <div className='img-box'>
-        <img src='/portrait.png' alt='portrait' />
+        <img src='/olha-portrait.png' alt='portrait' />
       </div>
     </div>
   )
